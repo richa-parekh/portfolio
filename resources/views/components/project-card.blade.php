cc7bb70daf0eec4cfa824974fb042950dbb7bcfb
Richa <richaparekh.work@gmail.com>
2026-06-08 22:45:16 +0530

    style: portfolio page responsive design

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -1,9 +1,10 @@
-<div class="bg-white rounded-3xl overflow-hidden shadow-sm hover:shadow-lg transition">
+<div class="bg-secondary-default overflow-hidden h-full">
 
     <img src="{{ asset('images/projects/' . $project->image) }}" alt="{{ $project->title }}" class="w-full h-60 object-cover" >
 
-    <div class="p-6">
-        <span class="text-sm text-blue-600 font-medium">
+    <div class="p-6 h-full">
+        
+        <span class="text-sm text-primary-600 font-medium bg-primary-100 px-2 py-1">
             {{ $project->project_type }}
         </span>
 
@@ -11,17 +12,30 @@
             {{ $project->title }}
         </h3>
 
-        <p class="text-gray-600 mt-3">
+        <p class="text-primary-400 mt-3">
             {{ $project->description }}
         </p>
 
-        <p class="mt-4 text-sm text-gray-500">
+        <!-- <p class="mt-4 text-sm text-gray-500">
             {{ $project->tech_stack }}
-        </p>
+        </p> -->
+        <div class="">
+            <a href="{{ $project->project_url }}" class="inline-flex items-center gap-3 mt-10 uppercase tracking-wide font-medium text-primary-default group/link text-sm">
+                Know More
+                <span
+                    class="w-6 h-6 rounded-full bg-primary-default text-secondary-default
+                    flex items-center justify-center
+                    transition duration-300 group-hover/link:rotate-45">
 
-        <a href="{{ $project->project_url }}" class="inline-block mt-5 text-blue-600">
+                    ↗
+                </span>
+
+            </a>
+        </div>
+        
+       <!--  <a href="{{ $project->project_url }}" class="inline-block mt-5 text-primary-600 font-medium hover:underline">
             View Project →
-        </a>
+        </a> -->
     </div>
     
 </div>
--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -3,11 +3,12 @@
 
 @section('content')
 
-<section class="py-20 md:py-24">
+<section class="bg-primary-default py-20 md:py-24">
     <div class="max-w-7xl mx-auto px-4 md:px-6">
         <x-section-heading
-            title="My Portfolio"
+            title="Portfolio"
             subtitle="Projects showcasing my web development work."
+            variant="secondary"
         />
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             @foreach($projects as $project)
