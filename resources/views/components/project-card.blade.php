f86e633b1a99c847699663f80180c1fc894d1802
Richa <richaparekh.work@gmail.com>
2026-06-09 22:10:05 +0530

    style: portfolio responsive design

--- resources/views/components/project-card.blade.php
+++ resources/views/components/project-card.blade.php
@@ -1,14 +1,14 @@
-<div class="bg-secondary-default overflow-hidden h-full">
+<div class="bg-secondary-default overflow-hidden flex flex-col h-full">
 
     <img src="{{ asset('images/projects/' . $project->image) }}" alt="{{ $project->title }}" class="w-full h-60 object-cover" >
 
-    <div class="p-6 h-full">
+    <div class="p-6 flex flex-col flex-1 items-start">
         
         <span class="text-sm text-primary-600 font-medium bg-primary-100 px-2 py-1">
             {{ $project->project_type }}
         </span>
 
-        <h3 class="text-2xl font-semibold mt-2">
+        <h3 class="text-2xl font-semibold mt-2 text-primary-600">
             {{ $project->title }}
         </h3>
 
@@ -19,7 +19,7 @@
         <!-- <p class="mt-4 text-sm text-gray-500">
             {{ $project->tech_stack }}
         </p> -->
-        <div class="">
+        <div class="mt-auto pt-8">
             <a href="{{ $project->project_url }}" class="inline-flex items-center gap-3 mt-10 uppercase tracking-wide font-medium text-primary-default group/link text-sm">
                 Know More
                 <span
